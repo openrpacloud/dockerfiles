f61c584d68772c4e3fa86cc4ede9731ad99a63d7
Chuanjian Wang <me@ckeyer.com>
2025-09-26 19:50:01 +0800

    fix

--- modelscope/download.py
+++ modelscope/download.py
@@ -3,7 +3,7 @@ import os
 # 指定模型
 model_name = os.getenv("MODEL_NAME", default="baichuan/t5-base")
 custom_path = os.getenv("MODEL_SAVE_PATH", default=".")
-revision = os.getenv("MODEL_REVISION", default="")
+revision = os.getenv("MODEL_REVISION", default=None)
 # model_dir = snapshot_download(model_name,cache_dir=custom_path)
 # print(f"model download to {model_dir}")
 
